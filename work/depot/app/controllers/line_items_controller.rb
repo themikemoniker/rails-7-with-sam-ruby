1673409a1d91cd865c5f8835840831e48617ee66
themikemoniker <themikemoniker@proton.me>
2025-07-16 15:26:38 -0600

    fixed issue with pages with no cart

--- work/depot/app/controllers/line_items_controller.rb
+++ work/depot/app/controllers/line_items_controller.rb
@@ -26,11 +26,10 @@ class LineItemsController < ApplicationController
     session[:counter] = 0
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
-    # @line_item = LineItem.new(line_item_params)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart) }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
